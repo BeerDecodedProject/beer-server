74f6ab22a82939c6896d6deccf9dff3449b1fc63
Xavier Stouder <xavier@stouder.io>
2017-02-10 16:00:14 +0100

    Add basic config system

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+const fs = require('fs')
+
+const config = JSON.parse(fs.readFileSync('config.json','utf8'))
+
 const express = require('express')
 const app = express()
 
 let router = express.Router()
 
 
-app.listen(80)
+app.listen(config.httpPort)
